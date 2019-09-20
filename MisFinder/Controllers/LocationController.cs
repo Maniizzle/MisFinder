8bc4d0819d7a227014fb4f4483d5ca57c7748303
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2019-09-20 12:57:35 +0100

    adding state Controller

---
+++ MisFinder/Controllers/LocationController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MisFinder.Data.Persistence.IRepositories;
+
+namespace MisFinder.Controllers
+{
+    public class StateController : Controller
+    {
+        private readonly IStateRepository repository;
+
+        public StateController(IStateRepository repository)
+        {
+            this.repository = repository;
+        }
+        public IEnumerable<States> GetAllStates()
+        {
+            repository.GetAllStates();
+        }
+    }
+}
