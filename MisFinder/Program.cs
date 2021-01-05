cc0224e74cba33cd390db83c8333689ac651960d
ONAKOYA OLAMIDE <olamideonakoya1@gmail.com>
2021-01-05 14:07:18 +0100

    finish db configuration

--- MisFinder/Program.cs
+++ MisFinder/Program.cs
@@ -6,21 +6,43 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using MisFinder.Data.Data.Context;
 
 namespace MisFinder
 {
     public class Program
     {
+        //public static void Main(string[] args)
+        //{
+        //    var host = CreateWebHostBuilder(args);
+        //    using (var scope = host.Services.CreateScope())
+        //    using (var context = scope.ServiceProvider.GetService<AppDbContext>())
+        //    {
+        //        context.Database.EnsureCreated();
+        //    }
+        //    CreateWebHostBuilder(args).Build().Run();
+        //}
+
+        //public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        //    WebHost.CreateDefaultBuilder(args)
+        //        .UseStartup<Startup>()
+        //    .UseDefaultServiceProvider(options=>options.ValidateScopes=false);
+
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            var host = BuildWebHost(args);
+            using (var scope = host.Services.CreateScope())
+            using (var context = scope.ServiceProvider.GetService<MisFinderDbContext>())
+            {
+                context.Database.EnsureCreated();
+            }
+            host.Run();
         }
 
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
+        public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>()
-            .UseDefaultServiceProvider(options=>options.ValidateScopes=false);
-            
+                .UseStartup<Startup>().UseDefaultServiceProvider(options => options.ValidateScopes = false).Build();
     }
 }
